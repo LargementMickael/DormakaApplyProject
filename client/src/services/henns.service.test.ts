2db1ec12d88ef555bc43b5b4ec4c34645cef00eb
Assuralink <mickael@assuralink.fr>
2022-01-01 22:55:27 +0100

    HennsService tests

--- client/src/services/henns.service.test.ts
+++ client/src/services/henns.service.test.ts
@@ -1,11 +1,61 @@
 import { hennsService } from './henns.service';
+import { loadingHennsResult, updatedHennResult, createdHennResult } from '../__mocksData__/fetchResults';
+
+beforeAll(() => {
+    global.fetch = jest.fn((
+        url: RequestInfo, 
+        optn: RequestInit
+    ) => {
+            if(typeof optn.method !== undefined){
+                switch(optn.method){
+                    case 'GET':
+                        return Promise.resolve({
+                            json: () => Promise.resolve(loadingHennsResult),
+                        });
+                    case 'POST':
+                        return Promise.resolve({
+                            json: () => Promise.resolve(createdHennResult),
+                        });
+                    case 'PATCH':
+                        return Promise.resolve({
+                            json: () => Promise.resolve(updatedHennResult),
+                        });
+                }
+            }
+        }
+    ) as jest.Mock;
+});
 
 describe("Henns Services API calls testing", () => {
 
-    const loadHenns = hennsService.loadHenns();
+    test("Loading henns endpoint", () => {
+        return hennsService.loadHenns().then(res => {
+            expect(res).toEqual(loadingHennsResult);
+        })
+    });
 
-    test("Loading henns endpoit", () => {
-            
+    test("Updating henn endpoint", () => {
+        return hennsService.updateHenn(
+            '', 
+            { 
+                name: '', 
+                breed: ''
+            }
+        ).then(res => {
+            expect(res).toEqual(updatedHennResult);
+        })
+    });
+
+    test("Creating henn endpoint", () => {
+        return hennsService.createHenn(
+            {
+                name: '',
+                breed: '',
+                imageUrl: ''
+            }
+        ).then(res => {
+            expect(res).toEqual(createdHennResult);
+        })
     });
 
 });
