d0561cacb01d862ca46ee82d7e97f328c154bf07
Assuralink <mickael@assuralink.fr>
2022-01-01 21:44:22 +0100

    CustomInputForm tests

--- client/src/components/CustomInputForm.test.tsx
+++ client/src/components/CustomInputForm.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Enzyme, { shallow, mount, render } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import CustomInputForm from "./CustomInputForm";
 import HennItem from './HennItem';
+import { errorMessages } from '../lib/errorMessages';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -27,6 +28,31 @@ describe("CustomInputForm component", () => {
 
     test("Should render HTML Element with 'InputForm' class", () => {
         expect(wrapper.find(".InputForm").exists()).toEqual(true);
+    });
+
+    test("ErrorMessage method should return message or nothing", () => {
+        expect(errorMessages.map(el => el.message)).toContain(wrapper.instance().errorMessage('name'));
+        expect(errorMessages.map(el => el.message)).toContain(wrapper.instance().errorMessage('breed'));
+    });
+
+    test("onChangeHandler should be called", () => {
+        wrapper.find("input").simulate('change', { target: { value: 'TEST' } });
+    });
+
+    test("resetInitialValue method should be called", () => {
+        wrapper.instance().resetInitialValue("initialValue");
+        expect(wrapper.state('value')).toEqual("initialValue");    
+    });
+
+    test("onChangeHandler method should be called", () => {
+        const wrapper = Enzyme.shallow<CustomInputForm>(<CustomInputForm 
+            name="name" 
+            label="Name" 
+            mode='EDIT'
+            value='InitValue'
+            onChange={jest.fn()}
+        />);  
+        wrapper.find("input").simulate('change', { target: { value: 'TEST' } });
     });
 
 });
---
+++ client/src/services/henns.service.test.ts
@@ -0,0 +1,11 @@
+import { hennsService } from './henns.service';
+
+describe("Henns Services API calls testing", () => {
+
+    const loadHenns = hennsService.loadHenns();
+
+    test("Loading henns endpoit", () => {
+            
+    });
+
+});
