867a6c903577f61dc0fd643098d16cc484cb2d3f
Assuralink <mickael@assuralink.fr>
2022-01-01 22:54:58 +0100

    HennsService test implementation

---
+++ client/src/__mocksData__/fetchResults.ts
@@ -0,0 +1,19 @@
+export const mockedHennData: Henn = {
+    _id: '',
+    name: 'MockedHenn',
+    breed: 'MockedBreed',
+}
+export const loadingHennsResult: Henn[] = [
+    mockedHennData
+];
+export const createdHennResult: Henn = {
+    _id: '',
+    name: 'NewHenn',
+    breed: 'NewBreed'
+}
+export const updatedHennResult: Henn = {
+    _id: '',
+    name: 'UpdatedHenn',
+    breed: 'UpdatedHenn'
+}
+
