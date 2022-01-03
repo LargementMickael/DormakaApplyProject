6725a090604e8321b5e79c3bc85eb49b9c04ac1f
Assuralink <mickael@assuralink.fr>
2022-01-03 01:09:47 +0100

    Removing useless lib and reorganize test dependencies

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,6 @@
 const config = {
     verbose: true,
     moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "jest-transform-stub",
       "\\.(css|less)$": "jest-transform-stub"
     }
 };
